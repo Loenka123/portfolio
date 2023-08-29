f8dd56846842aef9c7da6f95663a68009e77636a
Loenka123 <122457367+Loenka123@users.noreply.github.com>
2023-08-29 15:32:22 +0200

    About toegevoegd

--- projecten/index.php
+++ projecten/index.php
@@ -12,31 +12,42 @@
         <p class="portfolio">Portfolio</p>
         <div class="buttons">
             <a href="../index.html" class="home">Home</a>
+            <a href="../about/index.html" class="about">About</a>
             <a href="projecten/index.php" class="projecten">Projecten</a>
             <a href="../contact/index.html" class="contact">Contact</a>
         </div>
     </div>
-    <div class="row">
-    <?php
-    require_once "../db/db.php";
-    $statement = $db->query("SELECT * FROM projecten");
-    $counter = 0;
-    while ($row = $statement->fetch()) {
-        if ($counter % 3 == 0 && $counter > 0) {
-            echo '</div><div class="row">';
+    <h1 class="titel">Projecten</h1>
+    <p class="ondertitel">Luca Heintjes</p>
+    <div id="projecten">
+        <div class="row">
+        <?php
+        require_once "../db/db.php";
+        $statement = $db->query("SELECT * FROM projecten");
+        $counter = 0;
+        while ($row = $statement->fetch()) {
+            if ($counter % 3 == 0 && $counter > 0) {
+                ?>
+                </div><div class="row">
+                <?php
+            }
+            ?>
+            <div class="column">
+                <div class="project-container">
+                    <a href="<?= $row['link']?>">
+                    <div class="afbeelding-wrapper">
+                        <img src="<?= $row['foto']?>" alt="<?= $row['title']?>" draggable="false">
+                    </div>
+                    </a>
+                    <div class="project-naam"><?= $row['project']?></div>
+                    <div class="project-beschrijving"><?= $row['beschrijving']?></div>
+                </div>
+            </div>
+            <?php
+            $counter++;
         }
         ?>
-        <div class="column">
-            <div class="project-container">
-                <a href="<?= $row['link']?>"><img class="project" src="<?= $row['foto']?>" alt="<?= $row['title']?>"></a>
-                <div class="project-naam"><?= $row['project']?></div>
-                <div class="project-beschrijving"><?= $row['beschrijving']?></div>
-            </div>
         </div>
-        <?php
-        $counter++;
-    }
-    ?>
     </div>
 </body>
 </html>
