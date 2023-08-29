a45ea197dd7950378e8d6048449c584da17e1cb5
Loenka123 <122457367+Loenka123@users.noreply.github.com>
2023-08-29 11:52:47 +0200

    Contact update

--- contact/form.php
+++ contact/form.php
@@ -11,14 +11,6 @@ if (mail($email, $onderwerp, $bericht)) {
     echo "Er is iets fout gegaan bij het verzenden van de email.";
 }
 
-require_once "../db/db.php";
-$query = "INSERT INTO emails (email, onderwerp, bericht) VALUES (:email, :onderw, :beri)";
-$statement = $db->prepare($query);
-$statement->bindParam(":email", $voornaam);
-$statement->bindParam(":onderw", $onderw);
-$statement->bindParam(":beri", $beri);
-$statement->execute();
-
 $redirectUrl = "index.html";
 header("Location: " . $redirectUrl);
 exit();
